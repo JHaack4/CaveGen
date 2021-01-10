a9b0475e2995b33a1ad50d7620b12002d051face
JHaack4 <jhaack@hmc.edu>
2021-01-09 22:02:58 -0600

    _

--- continuous.py
+++ continuous.py
@@ -30,6 +30,7 @@ cap = cv2.VideoCapture(videoFile)
 if isinstance(args.camera, int):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_FPS, 30)
 
 if (cap.isOpened() == False): 
     print("Failure - error opening video stream")
